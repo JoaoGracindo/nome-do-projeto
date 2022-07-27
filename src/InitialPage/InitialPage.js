986b39d5f3ef6e1fc254e320797a76c3568693cd
JoaoGracindo <gabrielpessoa97@gmail.com.com>
2022-07-27 14:48:28 -0300

    axios get with use effect on InitialPage

--- src/InitialPage/InitialPage.js
+++ src/InitialPage/InitialPage.js
@@ -1,7 +1,30 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 
 export default function InitialPage (){
-    return <div>Bla</div>
+    const [movieList, setMovieList] = useState([])
+    useEffect(() =>{
+        const promise = axios.get('https://mock-api.driven.com.br/api/v5/cineflex/movies');
+        promise.then(value => {
+            setMovieList([...value.data]);
+        })
+        
+    },[])
+
+    function Movie ({id, posterURL, title}){
+        return(
+            <div>
+                <img src={posterURL} />
+            </div>
+        )
+    }
+
+
+    return(
+        <>
+            {movieList.map(value => <Movie {...value} />)}
+        </>
+    )
 }
