c9add8c03b932adfc0943b8568cf09561e0dff33
JoaoGracindo <gabrielpessoa97@gmail.com.com>
2022-07-27 09:37:12 -0300

    required libs for this project

---
+++ src/App/App.js
@@ -0,0 +1,10 @@
+import react from "react";
+import ReactDOM from 'react-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+function App (){
+    <>
+    </>
+}
+
+ReactDOM.render(App(),document.querySelector('.root'));
